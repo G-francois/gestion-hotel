00daaca92e4e5e1f7d4366874fc53114af06c578
G-François <francois.gnonhoue@gmail.com>
2023-07-20 05:23:53 +0200

    création de fichiers de réservation pour le client

--- app/client/site/reservation-suit.php
+++ app/client/site/reservation-suit.php
@@ -9,6 +9,8 @@ include './app/commum/header_client.php'
         <div class="row">
             <div class="card my-5 border-0 rounded-0">
                 <div class="row" style="background-color: #0c0b09">
+                    <h1 class="h3 mt-3 mb-3">Réservation d'une chambre SUITES</h1>
+
                     <div class="col-md-6">
                         <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                             <div class="carousel-inner">
@@ -49,7 +51,7 @@ include './app/commum/header_client.php'
                                     <i class="fas fa-vector-square"></i> 100m²
                                 </p>
                                 <p style="margin-left: 2rem">
-                                    <i class="bi bi-house"></i>590 €/nuit
+                                    <i class="bi bi-house"></i>50.000F/nuit
                                 </p>
                             </div>
                         </div>
@@ -67,18 +69,32 @@ include './app/commum/header_client.php'
                     <div class="col-md-6">
                         <div class="card-body px-0">
                             <h3>INFORMATIONS</h3>
-                            <form action="inscription-traitement.php" method="post" novalidate>
-
-                                <!-- Le champs nom -->
+                            <form action="<?= PATH_PROJECT ?>client/site/traitement-reservation-suit" method="post" class="user" novalidate>
+                                <!-- Le champs nom client -->
                                 <div class="col-sm-12 mb-3">
                                     <label for="inscription-nom"> Nom client:
                                         <span class="text-danger">(*)</span>
                                     </label>
 
-                                    <div class="input-group">
-                                        <input type="text" name="nom" id="inscription-nom" class="form-control" placeholder="Veuillez entrer votre nom" value="<?= (isset($donnees["nom"]) && !empty($donnees["nom"])) ? $donnees["nom"] : ""; ?>" required>
-                                    </div>
+                                    <?php
+                                    if (!check_if_user_connected_client()) {
+                                    ?>
+                                        <div class="input-group">
+                                            <input type="text" name="nom" id="inscription-nom" class="form-control" placeholder="Veuillez entrer votre nom" value="<?= (isset($donnees["nom"]) && !empty($donnees["nom"])) ? $donnees["nom"] : ""; ?>" required>
+                                        </div>
+                                    <?php
+                                    }
+                                    ?>
 
+                                    <?php
+                                    if (check_if_user_connected_client()) {
+                                    ?>
+                                        <div class="input-group">
+                                            <input name="nom" type="text" class="form-control <?= isset($_SESSION['erreurs']['nom']) ? 'is-invalid' : '' ?>" id="Name" value="<?= isset($_SESSION['utilisateur_connecter_client']) ?  $_SESSION['utilisateur_connecter_client']['nom'] : 'Nom' ?> <?= isset($_SESSION['utilisateur_connecter_client']) ?  $_SESSION['utilisateur_connecter_client']['prenom'] : 'Prenom' ?>">
+                                        </div>
+                                    <?php
+                                    }
+                                    ?>
                                     <span class="text-danger">
                                         <?php
                                         if (isset($erreurs["nom"]) && !empty($erreurs["nom"])) {
@@ -168,18 +184,18 @@ include './app/commum/header_client.php'
 
                                 <!-- Le champs date de  début occupation -->
                                 <div class="col-sm-12 mb-3">
-                                    <label for="inscription-date-reservation">
+                                    <label for="inscription-date-debut">
                                         Date de départ occupation:
                                         <span class="text-danger">(*)</span>
                                     </label>
                                     <div class="input-group mb-3">
-                                        <input type="date" name="date-reservation" id="inscription-reservation" class="form-control" placeholder="Veuillez entrer votre date de début occupation" value="<?= (isset($donnees["date-naissance"]) && !empty($donnees["date-naissance"])) ? $donnees["date-naissance"] : ""; ?>" required>
+                                        <input type="date" name="date-debut" id="inscription-date-debut" class="form-control" placeholder="Veuillez entrer votre date de début occupation" value="<?= (isset($donnees["date-debut"]) && !empty($donnees["date-debut"])) ? $donnees["date-debut"] : ""; ?>" required>
                                     </div>
 
                                     <span class="text-danger">
                                         <?php
-                                        if (isset($erreurs["date"]) && !empty($erreurs["date-reservation"])) {
-                                            echo $erreurs["date"];
+                                        if (isset($erreurs["date-debut"]) && !empty($erreurs["date-debut"])) {
+                                            echo $erreurs["date-debut"];
                                         }
                                         ?>
                                     </span>
@@ -187,23 +203,24 @@ include './app/commum/header_client.php'
 
                                 <!-- Le champs date de  fin occupation -->
                                 <div class="col-sm-12 mb-3">
-                                    <label for="inscription-date-reservation">
+                                    <label for="inscription-date-fin">
                                         Date de fin occupation:
                                         <span class="text-danger">(*)</span>
                                     </label>
                                     <div class="input-group mb-3">
-                                        <input type="date" name="date-reservation" id="inscription-reservation" class="form-control" placeholder="Veuillez entrer votre date de fin occupation" value="<?= (isset($donnees["date-naissance"]) && !empty($donnees["date-naissance"])) ? $donnees["date-naissance"] : ""; ?>" required>
+                                        <input type="date" name="date-fin" id="inscription-date-fin" class="form-control" placeholder="Veuillez entrer votre date de fin occupation" value="<?= (isset($donnees["date-fin"]) && !empty($donnees["date-fin"])) ? $donnees["date-fin"] : ""; ?>" required>
                                     </div>
 
                                     <span class="text-danger">
                                         <?php
-                                        if (isset($erreurs["date"]) && !empty($erreurs["date-reservation"])) {
-                                            echo $erreurs["date"];
+                                        if (isset($erreurs["date-fin"]) && !empty($erreurs["date-fin"])) {
+                                            echo $erreurs["date-fin"];
                                         }
                                         ?>
                                     </span>
                                 </div>
 
+                                <!-- Le champs numeros de chambre -->
                                 <div class="col-sm-12 mb-3">
                                     <label for="nom-auteur" class="col-sm-4 col-form-label">Numéros de chambre
                                         <span class="text-danger">(*)</span>
@@ -211,38 +228,26 @@ include './app/commum/header_client.php'
                                     <div class="input-group">
                                         <select class="form-control" name="type" id="type">
                                             <option value="" disabled selected>Veuillez choisir le numéro de chambre</option>
-                                            <option value="">A-101</option>
-                                            <option value="">A-102</option>
-                                            <option value="">A-103</option>
-                                            <option value="">A-104</option>
-                                            <option value="">B-101</option>
-                                            <option value="">B-102</option>
-                                            <option value="">B-103</option>
-                                            <option value="">B-104</option>
-                                            <option value="">C-101</option>
-                                            <option value="">C-102</option>
-                                            <option value="">C-103</option>
-                                            <option value="">C-104</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="col-sm-12 mb-3">
-                                    <label for="nom-auteur" class="col-sm-4 col-form-label">Mode de paiement</label>
-                                    <div class="input-group">
-                                        <select class="form-control" name="type" id="type">
-                                            <option value="" disabled selected>Veuillez choisir le mode de paiement</option>
-                                            <option value="">Carte de crédit</option>
-                                            <option value="">Monnaies virtuelles</option>
-                                            <option value="">Mobile</option>
+                                            <?php
+                                            $chambres = recuperer_liste_chambres();
+                                            foreach ($chambres as $chambre) {
+                                                if ($chambre['lib_typ'] === 'Suites') {
+                                                    echo '<option value="' . $chambre['num_chambre'] . '">' . $chambre['num_chambre'] . '</option>';
+                                                }
+                                            }
+                                            ?>
                                         </select>
                                     </div>
                                 </div>
 
                                 <div class="col-lg-12">
-                                    <h5 style="font-weight: bold">Total Days : <span id="staying_day">0</span> Days</h5>
-                                    <h5 style="font-weight: bold">Price: /-</h4>
-                                        <h5 style="font-weight: bold">Total Amount : <span id="total_price">0</span> /-</h5>
+                                    <h5 style="font-weight: bold">Nombre total de jours : <span id="staying_day">0</span></h5>
+                                    <h5 style="font-weight: bold">Montant total : <span id="total_price">0</span> F</h5>
+                                </div>
+
+                                <div class="float-right" style="text-align: right;">
+                                    <button type="reset" class="btn btn-danger">Annuler</button>
+                                    <button type="submit" class="btn btn-success">Enregistrer</button>
                                 </div>
                         </div>
                     </div>
@@ -251,8 +256,56 @@ include './app/commum/header_client.php'
         </div>
     </div>
 </section>
+
+<script>
+    function calculateStayDuration() {
+        var dateDebut = document.getElementById('inscription-date-debut').value;
+        var dateFin = document.getElementById('inscription-date-fin').value;
+
+        // Vérifier si les champs de date de début et de date de fin sont remplis
+        if (dateDebut && dateFin) {
+            var startDate = new Date(dateDebut);
+            var endDate = new Date(dateFin);
+
+            // Calculer la différence en millisecondes
+            var diff = Math.abs(endDate - startDate);
+
+            // Convertir la différence en jours
+            var nbJours = Math.ceil(diff / (1000 * 60 * 60 * 24));
+
+            // Afficher le nombre de jours dans l'élément avec l'ID "staying_day"
+            document.getElementById('staying_day').textContent = nbJours;
+
+            // Mettre à jour le prix total en fonction du nombre de jours
+            updateTotalPrice(nbJours);
+        } else {
+            // Les champs de date de début et de date de fin ne sont pas remplis, réinitialiser les valeurs affichées
+            document.getElementById('staying_day').textContent = '0';
+            updateTotalPrice(0);
+        }
+    }
+
+    function updateTotalPrice(nbJours) {
+        var prixParNuit = 50000; // Prix par nuit pour la chambre Solo
+        var total = prixParNuit * nbJours;
+
+        // Ajouter un point après chaque groupe de 3 chiffres
+        var formattedTotal = total.toLocaleString();
+
+        // Afficher le prix total dans l'élément avec l'ID "total_price"
+        document.getElementById('total_price').textContent = formattedTotal + " /-";
+    }
+
+    // Écouter les événements de changement de date de début et de date de fin
+    document.getElementById('inscription-date-debut').addEventListener('change', calculateStayDuration);
+    document.getElementById('inscription-date-fin').addEventListener('change', calculateStayDuration);
+
+    // Calculer la durée du séjour au chargement initial de la page
+    calculateStayDuration();
+</script>
+
 <?php
 
-include './app/commum/footer_client.php'
+include './app/commum/footer_client.php';
 
 ?>
